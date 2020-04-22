1c80181b173e5701023c91de0820d5a4fdc753fa
v0latileDev <64113623+v0latileDev@users.noreply.github.com>
2020-04-21 21:18:38 -0400

    Create LocalScript.lua

---
+++ StarterGui/LocalScript.lua
@@ -0,0 +1,9 @@
+local Tree_Module = require(game.ReplicatedStorage.Tree2)
+local TreeFolder = game.Workspace.TreeFolder:GetChildren()
+
+for i = 1, #TreeFolder do
+	local Start = TreeFolder[i]
+
+	local Tree = Tree_Module.new()
+	Tree:Branch(Start, 10)
+end
